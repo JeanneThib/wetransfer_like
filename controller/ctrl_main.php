7584d10f0eac9e080e359da27d4a6f19c282e1b5
vagrant <vagrant@vagrant.vm>
2018-12-10 14:52:00 +0000

    Template lien entre model/controller/view

--- controller/ctrl_main.php
+++ controller/ctrl_main.php
@@ -4,4 +4,14 @@ require_once 'vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('view');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('view_main.twig');
+
+function myFunction() {
+    global $twig;
+
+    echo $twig->render('view_main.twig',
+    array('test' => $test));
+};
+
+myFunction();
+
+?>
