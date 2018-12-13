cb286b6ea6d20097289facc2c9f680fa28bc935c
JeanneThib <jeanne.t@codeur.online>
2018-12-13 09:20:00 +0100

    Préparation du mail html en php

--- controller/ctrl_upload.php
+++ controller/ctrl_upload.php
@@ -116,12 +116,12 @@ $message = "<html><head><title></title>
       }
     }
   
-  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
-  <style type="text/css">
+  
+  
     @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
-  </style>
-  <!--<![endif]-->
-  <style type="text/css">
+  
+  
+  
     @media only screen and (min-width:480px) {
       .mj-column-px-600 {
         width: 600px !important;
@@ -139,21 +139,21 @@ $message = "<html><head><title></title>
       }
     }
   </style>
-  <style type="text/css">
+  
   </style>
 </head>
 
 <body>
-  <div style="">
+  <div style>
    
-    <div style="background:#318ce7;background-color:#318ce7;Margin:0px auto;max-width:600px;">
-      <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#318ce7;background-color:#318ce7;width:100%;">
+    <div style=background:#318ce7;background-color:#318ce7;Margin:0px auto;max-width:600px;>
+      <table align=center border=0 cellpadding=0 cellspacing=0 role=presentation style=background:#318ce7;background-color:#318ce7;width:100%;>
         <tbody>
           <tr>
-            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
+            <td style=direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;>
          
-              <div class="mj-column-px-600 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+              <div class=mj-column-px-600 outlook-group-fix style=font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;>
+                <table border=0 cellpadding=0 cellspacing=0 role=presentation style=vertical-align:top;width=100%>
                 </table>
               </div>
             </td>
@@ -161,16 +161,16 @@ $message = "<html><head><title></title>
         </tbody>
       </table>
     </div>
-    <div style="background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;">
-      <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#fafafa;background-color:#fafafa;width:100%;">
+    <div style=background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;>
+      <table align=center border=0 cellpadding=0 cellspacing=0 role=presentation style=background:#fafafa;background-color:#fafafa;width:100%;>
         <tbody>
           <tr>
-            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-              <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+            <td style=direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;>
+              <div class=mj-column-per-100 outlook-group-fix style=font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;>
+                <table border=0 cellpadding=0 cellspacing=0 role=presentation style=vertical-align:top; width=100%>
                   <tr>
-                    <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:20px;font-style:italic;line-height:1;text-align:center;color:#626262;"> We transfer vous informes </div>
+                    <td align=center style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:20px;font-style:italic;line-height:1;text-align:center;color:#626262;> We transfer vous informes </div>
                     </td>
                   </tr>
                 </table>
@@ -180,37 +180,37 @@ $message = "<html><head><title></title>
         </tbody>
       </table>
     </div>
-    <div style="background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;">
-      <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#fafafa;background-color:#fafafa;width:100%;">
+    <div style=background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;>
+      <table align=center border=0 cellpadding=0 cellspacing=0 role=presentation style=background:#fafafa;background-color:#fafafa;width:100%;>
         <tbody>
           <tr>
-            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-              <div class="mj-column-px-400 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+            <td style=direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;>
+              <div class=mj-column-px-400 outlook-group-fix style=font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;>
+                <table border=0 cellpadding=0 cellspacing=0role=presentation style=vertical-align:top; width=100%>
                   <tr>
-                    <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Helvetica Neue;font-size:20px;font-style:italic;line-height:1;text-align:left;color:#626262;"> Bonjour, </div>
+                    <td align=left style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Helvetica Neue;font-size:20px;font-style:italic;line-height:1;text-align:left;color:#626262;> Bonjour, </div>
                     </td>
                   </tr>
                   <tr>
-                    <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#525252;"> Veuillez trouver ci joint le lien pour télécharger vos fichiers ou copier/coller le lien dans votre navigateur internet </div>
+                    <td align=left style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#525252;> Veuillez trouver ci joint le lien pour télécharger vos fichiers ou copier/coller le lien dans votre navigateur internet </div>
                     </td>
                   </tr>
                   <tr>
-                    <td align="center" vertical-align="middle" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
+                    <td align=center vertical-align=middle style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <table border=0 cellpadding=0 cellspacing=0 role=presentation style=border-collapse:separate;line-height:100%;>
                         <tr>
-                          <td align="center" bgcolor="#318ce7" role="presentation" style="border:none;border-radius:3px;cursor:auto;padding:10px 25px;background:#318ce7;" valign="middle">
-                            <a href="#" style="background:#318ce7;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:120%;Margin:0;text-decoration:none;text-transform:none;" target="_blank"> Lien de téléchargement </a>
+                          <td align=center bgcolor=#318ce7 role=presentation style=border:none;border-radius:3px;cursor:auto;padding:10px 25px;background:#318ce7; valign=middle>
+                            <a href=# style=background:#318ce7;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:120%;Margin:0;text-decoration:none;text-transform:none; target=_blank> Lien de téléchargement </a>
                           </td>
                         </tr>
                       </table>
                     </td>
                   </tr>
                   <tr>
-                    <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:10px;line-height:1;text-align:center;color:#000000;"> https://www.sois-net.fr/mjml-framework-emails-responsive/ </div>
+                    <td align=center style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:10px;line-height:1;text-align:center;color:#000000;> https://www.sois-net.fr/mjml-framework-emails-responsive/ </div>
                     </td>
                   </tr>
                 </table>
@@ -221,21 +221,21 @@ $message = "<html><head><title></title>
         </tbody>
       </table>
     </div>
-    <div style="background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;">
-      <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#fafafa;background-color:#fafafa;width:100%;">
+    <div style=background:#fafafa;background-color:#fafafa;Margin:0px auto;max-width:600px;>
+      <table align=center border=0 cellpadding=0 cellspacing=0 role=presentation style=background:#fafafa;background-color:#fafafa;width:100%;>
         <tbody>
           <tr>
-            <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-              <div class="mj-column-px-400 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+            <td style=direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;>
+              <div class=mj-column-px-400 outlook-group-fix style=font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;>
+                <table border=0 cellpadding=0 cellspacing=0 role=presentation style=vertical-align:top; width=100%>
                   <tr>
-                    <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Helvetica Neue;font-size:12px;line-height:1;text-align:left;color:#626262;"> Nous vous remercions pour votre confiance </div>
+                    <td align=left style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Helvetica Neue;font-size:12px;line-height:1;text-align:left;color:#626262;> Nous vous remercions pour votre confiance </div>
                     </td>
                   </tr>
                   <tr>
-                    <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                      <div style="font-family:Helvetica Neue;font-size:10px;line-height:1;text-align:left;color:#525252;"> Et ne vous inquiétez pas aucun de vos fichiers ou de vos données ne seront transférés à d'autre personnes , nous mettons tout en oeuvre pour vous protéger et sécurisé vos données </div>
+                    <td align=left style=font-size:0px;padding:10px 25px;word-break:break-word;>
+                      <div style=font-family:Helvetica Neue;font-size:10px;line-height:1;text-align:left;color:#525252;> Et ne vous inquiétez pas aucun de vos fichiers ou de vos données ne seront transférés à d'autre personnes , nous mettons tout en oeuvre pour vous protéger et sécurisé vos données </div>
                     </td>
                   </tr>
                 </table>
@@ -247,13 +247,25 @@ $message = "<html><head><title></title>
     </div>
   </div>
 </body>
-</html>"
+</html>";
 
 
+$passage_ligne = "\n";
+$boundary = "-----=".md5(rand());
+$header = "From: \"Moi-même\"<testdemail@mail.fr>".$passage_ligne;
+$header.= "Reply-to: \"WeaponsB\" <weaponsb@mail.fr>".$passage_ligne;
+$header.= "MIME-Version: 1.0".$passage_ligne;
+$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+$message = $passage_ligne."--".$boundary.$passage_ligne;
+$message.= $passage_ligne."--".$boundary.$passage_ligne;
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
 
+$header = "From:" . $from;
 
-$headers = "From:" . $from;
-
-mail($to,$subject,$message, $headers);
+mail($to,$subject,$message, $header);
 
 ?>
