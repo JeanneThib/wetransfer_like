b880df946da54066e0b568e0c466ade44b2c3b79
vagrant <vagrant@vagrant.vm>
2018-12-13 08:14:16 +0000

    merge master into antonin

--- model/mdl_upload.php
+++ model/mdl_upload.php
@@ -3,8 +3,8 @@
 require_once "connect.php";
 function insertDB($name, $url, $date, $fileSize, $ext){
     global $bdd;
-    
-    $sql = "INSERT INTO file_upload (name,path,link_id,upload_date,size,extension) VALUES (:name,'../cloud/',:url,:date,:fileSize,:ext)" ;
+
+    $sql = "INSERT INTO file_upload (name,path,link_id,upload_date,size,extension) VALUES (:name,'../cloud/',:url,:date,:fileSize,:ext)";
     
     $response = $bdd->prepare( $sql );
     $response->bindParam(':name', $name, PDO::PARAM_STR);
