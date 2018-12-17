a2df18ddbc33f8cd2b942cb9fa77c89ba7b173bc
JeanneThib <jeanne.t@codeur.online>
2018-12-17 13:33:27 +0100

    ajout du lien à copier coller

--- model/connect.php
+++ model/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$base_url = (strstr($_SERVER["HTTP_HOST"],"8080")=== false)?"https://antoninl.promo-23.codeur.online/wetransfer_like/":"https://antoninl.promo-23.codeur.online:8080/wetransfer_like/";
-$username ='antoninl';
-$password ='qVDu8KtG3xTbpw==';
-$database ='antoninl_db_transfert';
+$base_url = (strstr($_SERVER["HTTP_HOST"],"8080")=== false)?"http://localhost/wetransfer_like/":"http://localhost:8080/wetransfer_like/";
+$username ='root';
+$password ='online@2017';
+$database ='db_transfert';
 $host ='localhost';
 
 try{
