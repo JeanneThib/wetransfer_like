c88c81678bf09468ee5bc2a3825b0d70928bc179
vagrant <vagrant@vagrant.vm>
2018-12-19 09:33:31 +0000

    Download OK

--- controller/ctrl_download.php
+++ controller/ctrl_download.php
@@ -10,6 +10,10 @@ switch ($action) {
     case 'show':
         showDownload();
         break;
+
+    case 'download':
+        getFile();
+        break;
     
     default:
         show404();
@@ -34,32 +38,41 @@ function showDownload() {
             $extension = $key['extension'];
             $link_id = $key['link_id'];
         }
-    
-    echo $lien = $link_id.'.'.$extension;
 
     // Chemin du fichier à télécharger
     $filepath = $_SERVER['DOCUMENT_ROOT'].'/wetransfer_like/cloud/'.$link_id.'.'.$extension;
 
     // Render twig de la page
-    echo $twig->render('view_download.twig', array('chemin'=> '../../cloud/'.$lien));
-    
-    // Quand interaction avec le input name="bouton"
-        if(isset($_POST['bouton'])){
-
-            // Code pour téléchargement
-            header('Content-Disposition: attachmeshow/1545059594455-998b65c4b23ecf215083806abaa4c019c360af7bnt; filename="'.basename($filepath).'"');
-            header('Cache-Control: must-revalidate');
-            header('Content-Type: application/octet-stream');
-            header('Content-Length: '.filesize($filepath));
-            header('Expires: 0');
-            flush(); // Flush system output buffer
-            header('Pragma: public');
-            ob_clean();
-            readfile($filepath);
-            exit();
-        }
+    echo $twig->render('view_download.twig', array('chemin'=> '../download/'.$link_id));
 }
 
+function getFile(){
+       
+    // Requete SQL avec identifiant du fichier
+    $file = getFile (substr(  strrchr($_SERVER['REQUEST_URI'], '/')  ,1));
+    
+        // Boucle pour parcourir le tableau de tableaux et récupérer les données de la BDD
+        foreach ($file as $key) {
+            $extension = $key['extension'];
+            $link_id = $key['link_id'];
+        }
+
+    // Chemin du fichier à télécharger
+    $filepath = $_SERVER['DOCUMENT_ROOT'].'/wetransfer_like/cloud/'.$link_id.'.'.$extension;
+
+        // Code pour téléchargement
+        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+        header('Cache-Control: must-revalidate');
+        header('Content-Type: application/octet-stream');
+        header('Content-Length: '.filesize($filepath));
+        header('Expires: 0');
+        flush(); // Flush system output buffer
+        header('Pragma: public');
+        ob_clean();
+        readfile($filepath);
+        exit();
+    
+}
 
 // function myFunction() {
 //     global $twig;
