9ecd50132756e30601c24da6893702821625d600
vagrant <vagrant@vagrant.vm>
2018-12-20 15:33:58 +0000

    commit before merge

--- controller/ctrl_upload.php
+++ controller/ctrl_upload.php
@@ -85,7 +85,7 @@ function verifUpload(){
 
         // Si $resultat = true
         if ($resultat){
-            $dlLink = 'https://jeannet.promo-23.codeur.online/wetransfer_like/download/show/' . $full;
+            $dlLink = 'https://antoninl.promo-23.codeur.online/wetransfer_like/download/show/' . $full;
         };
 
         // ===== ENVOI BDD =====
@@ -111,6 +111,7 @@ function verifUpload(){
             }
 
         } else {
+
             $to = '"';
         
         }
